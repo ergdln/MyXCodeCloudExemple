2a411de8786cce9ed2222b52d0317aab209c2eb0
Ernesto <elgn@cin.ufpe.br>
2023-02-23 16:20:36 -0300

    initial commit

---
+++ MyXCodeCloudExemple/MyXCodeCloudExemple/MyXCodeCloudExempleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyXCodeCloudExempleApp.swift
+//  MyXCodeCloudExemple
+//
+//  Created by egln on 23/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MyXCodeCloudExempleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
